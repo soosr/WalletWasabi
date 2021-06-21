8d69e5aa6bdf43adf30e0392bf4c6061383f43a9
Roland Soós <soos.roland93@gmail.com>
2021-06-21 14:25:49 +0200

    put loading logic into LoadingControlViewModel.cs

--- WalletWasabi.Fluent/ViewModels/Wallets/LoadingControlViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/LoadingControlViewModel.cs
@@ -1,14 +1,78 @@
+using System;
+using System.Diagnostics;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI;
+using WalletWasabi.Blockchain.Blocks;
+using WalletWasabi.Fluent.Helpers;
+using WalletWasabi.Wallets;
+
 namespace WalletWasabi.Fluent.ViewModels.Wallets
 {
-	public partial class LoadingControlViewModel : ViewModelBase
+	public partial class LoadingControlViewModel : ActivatableViewModel
 	{
+		private readonly Wallet _wallet;
+
 		[AutoNotify] private double _percent;
 		[AutoNotify] private string? _statusText;
 
-		public LoadingControlViewModel()
+		private uint? _startingFilterIndex;
+		private Stopwatch? _stopwatch;
+
+		public LoadingControlViewModel(Wallet wallet)
 		{
+			_wallet = wallet;
 			_statusText = "";
 			_percent = 0;
+		}
+
+		protected override void OnActivated(CompositeDisposable disposables)
+		{
+			base.OnActivated(disposables);
+
+			_stopwatch ??= Stopwatch.StartNew();
+
+			Observable.Interval(TimeSpan.FromSeconds(1))
+				.ObserveOn(RxApp.MainThreadScheduler)
+				.Subscribe(_ =>
+				{
+					var segwitActivationHeight = SmartHeader.GetStartingHeader(_wallet.Network).Height;
+					if (_wallet.LastProcessedFilter?.Header?.Height is { } lastProcessedFilterHeight
+					    && lastProcessedFilterHeight > segwitActivationHeight
+					    && Services.BitcoinStore.SmartHeaderChain.TipHeight is { } tipHeight
+					    && tipHeight > segwitActivationHeight)
+					{
+						var allFilters = tipHeight - segwitActivationHeight;
+						var processedFilters = lastProcessedFilterHeight - segwitActivationHeight;
+
+						UpdateStatus(allFilters, processedFilters, _stopwatch.ElapsedMilliseconds);
+					}
+				})
+				.DisposeWith(disposables);
+		}
+
+		private void UpdateStatus(uint allFilters, uint processedFilters, double elapsedMilliseconds)
+		{
+			var percent = (decimal) processedFilters / allFilters * 100;
+			_startingFilterIndex ??= processedFilters; // Store the filter index we started on. It is needed for better remaining time calculation.
+			var realProcessedFilters = processedFilters - _startingFilterIndex.Value;
+			var remainingFilterCount = allFilters - processedFilters;
+
+			var tempPercent = (uint) Math.Round(percent);
+
+			if (tempPercent == 0 || realProcessedFilters == 0 || remainingFilterCount == 0)
+			{
+				return;
+			}
+
+			Percent = tempPercent;
+			var percentText = $"{Percent}% completed";
+
+			var remainingMilliseconds = elapsedMilliseconds / realProcessedFilters * remainingFilterCount;
+			var userFriendlyTime = TextHelpers.TimeSpanToFriendlyString(TimeSpan.FromMilliseconds(remainingMilliseconds));
+			var remainingTimeText = string.IsNullOrEmpty(userFriendlyTime) ? "" : $"- {userFriendlyTime} remaining";
+
+			StatusText = $"{percentText} {remainingTimeText}";
 		}
 	}
 }
