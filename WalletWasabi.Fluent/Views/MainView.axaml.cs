27e348302670128a70e0570fb9279aba01a85701
Roland Soós <soos.roland93@gmail.com>
2021-09-20 16:11:38 +0200

    repro code

--- WalletWasabi.Fluent/Views/MainView.axaml.cs
+++ WalletWasabi.Fluent/Views/MainView.axaml.cs
@@ -1,5 +1,10 @@
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using ReactiveUI;
+using WalletWasabi.Fluent.ViewModels;
 
 namespace WalletWasabi.Fluent.Views
 {
@@ -13,6 +18,16 @@ namespace WalletWasabi.Fluent.Views
 		private void InitializeComponent()
 		{
 			AvaloniaXamlLoader.Load(this);
+		}
+
+		private void Button_OnClick(object? sender, RoutedEventArgs e)
+		{
+			RxApp.MainThreadScheduler.Schedule(async () =>
+			{
+				DataContext = null;
+				await Task.Delay(2000);
+				DataContext = MainViewModel.Instance;
+			});
 		}
 	}
 }
