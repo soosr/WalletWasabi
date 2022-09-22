1a8a5c987fa0d212faf40585ad518fff76fb12d0
Roland Soós <soos.roland93@gmail.com>
2022-09-22 13:00:39 +0200

    add tests

--- WalletWasabi.Tests/UnitTests/Wallet/SmartCoinSelectorTests.cs
+++ WalletWasabi.Tests/UnitTests/Wallet/SmartCoinSelectorTests.cs
@@ -6,6 +6,7 @@ using WalletWasabi.Blockchain.Keys;
 using WalletWasabi.Blockchain.TransactionBuilding;
 using WalletWasabi.Blockchain.TransactionOutputs;
 using WalletWasabi.Tests.Helpers;
+using WalletWasabi.Tests.UnitTests.UserInterfaceTest;
 using Xunit;
 
 namespace WalletWasabi.Tests.UnitTests.Wallet;
@@ -30,7 +31,7 @@ public class SmartCoinSelectorTests
 			Enumerable.Range(0, 9).Select(i => ("Juan", 0.1m * (i + 1))))
 			.ToList();
 
-		var selector = new SmartCoinSelector(availableCoins, privateThreshold: 100);
+		var selector = new SmartCoinSelector(availableCoins, privateThreshold: 100, recipient: SmartLabel.Empty);
 		var coinsToSpend = selector.Select(Enumerable.Empty<Coin>(), Money.Coins(0.3m));
 
 		var theOnlyOne = Assert.Single(coinsToSpend.Cast<Coin>());
@@ -46,7 +47,7 @@ public class SmartCoinSelectorTests
 
 		smartCoins.Add(BitcoinFactory.CreateSmartCoin(smartCoins[0].HdPubKey, 0.11m));
 
-		var selector = new SmartCoinSelector(smartCoins, privateThreshold: 100);
+		var selector = new SmartCoinSelector(smartCoins, privateThreshold: 100, recipient: SmartLabel.Empty);
 
 		var someCoins = smartCoins.Select(x => x.Coin);
 		var coinsToSpend = selector.Select(someCoins, Money.Coins(0.41m));
@@ -62,7 +63,7 @@ public class SmartCoinSelectorTests
 
 		smartCoins.Add(BitcoinFactory.CreateSmartCoin(smartCoins[0].HdPubKey, 0.11m));
 
-		var selector = new SmartCoinSelector(smartCoins, privateThreshold: 100);
+		var selector = new SmartCoinSelector(smartCoins, privateThreshold: 100, recipient: SmartLabel.Empty);
 
 		var coinsToSpend = selector.Select(Enumerable.Empty<Coin>(), Money.Coins(0.31m)).Cast<Coin>().ToList();
 
@@ -78,11 +79,100 @@ public class SmartCoinSelectorTests
 
 		var coinsKnownByBeto = GenerateSmartCoins(Enumerable.Repeat(("Beto", 0.2m), 2));
 
-		var selector = new SmartCoinSelector(coinsKnownByJuan.Concat(coinsKnownByBeto).ToList(), privateThreshold: 100);
+		var selector = new SmartCoinSelector(coinsKnownByJuan.Concat(coinsKnownByBeto).ToList(), privateThreshold: 100, recipient: SmartLabel.Empty);
 		var coinsToSpend = selector.Select(Enumerable.Empty<Coin>(), Money.Coins(0.3m)).Cast<Coin>().ToList();
 
 		Assert.Equal(2, coinsToSpend.Count);
 		Assert.Equal(0.4m, coinsToSpend.Sum(x => x.Amount.ToUnit(MoneyUnit.BTC)));
+	}
+
+	[Fact]
+	public void PreferPrivateCoins()
+	{
+		var privateThreshold = 100;
+		var amountToSend = Money.Coins(0.6m);
+
+		var smartCoins = new List<SmartCoin>()
+		{
+			LabelTestExtensions.CreateCoin(0.5m, "Juan", anonymitySet: 150),
+			LabelTestExtensions.CreateCoin(0.3m, "Beto", anonymitySet: 150),
+			LabelTestExtensions.CreateCoin(0.8m, "Juan", anonymitySet: 1),
+			LabelTestExtensions.CreateCoin(0.6m, "Beto", anonymitySet: 1),
+		};
+		var privateCoins = smartCoins.Where(x => x.HdPubKey.AnonymitySet > privateThreshold).Select(x => x.Coin);
+
+		var selector = new SmartCoinSelector(smartCoins, privateThreshold, recipient: "");
+		var coinsToSpend = selector.Select(Enumerable.Empty<Coin>(), amountToSend).Cast<Coin>().ToList();
+
+		Assert.True(coinsToSpend.All(coin => privateCoins.Contains(coin)));
+	}
+
+	[Fact]
+	public void PreferSemiPrivateCoins()
+	{
+		var privateThreshold = 100;
+		var amountToSend = Money.Coins(1.0m);
+
+		var smartCoins = new List<SmartCoin>()
+		{
+			LabelTestExtensions.CreateCoin(0.5m, "Juan", anonymitySet: 150),
+			LabelTestExtensions.CreateCoin(0.3m, "Beto", anonymitySet: 5),
+			LabelTestExtensions.CreateCoin(0.8m, "Juan", anonymitySet: 5),
+			LabelTestExtensions.CreateCoin(1.6m, "Beto", anonymitySet: 1),
+		};
+		var semiPrivateCoins = smartCoins.Where(x => x.HdPubKey.AnonymitySet < privateThreshold && x.HdPubKey.AnonymitySet >= 2).Select(x => x.Coin);
+
+		var selector = new SmartCoinSelector(smartCoins, privateThreshold, recipient: "");
+		var coinsToSpend = selector.Select(Enumerable.Empty<Coin>(), amountToSend).Cast<Coin>().ToList();
+
+		Assert.True(coinsToSpend.All(coin => semiPrivateCoins.Contains(coin)));
+	}
+
+	[Fact]
+	public void PreferSemiPrivateAndPrivateCoins()
+	{
+		var privateThreshold = 100;
+		var amountToSend = Money.Coins(0.6m);
+
+		var smartCoins = new List<SmartCoin>()
+		{
+			LabelTestExtensions.CreateCoin(0.5m, "Juan", anonymitySet: 150),
+			LabelTestExtensions.CreateCoin(0.3m, "Beto", anonymitySet: 5),
+			LabelTestExtensions.CreateCoin(0.8m, "Juan", anonymitySet: 1),
+			LabelTestExtensions.CreateCoin(1.6m, "Beto", anonymitySet: 1),
+			LabelTestExtensions.CreateCoin(0.3m, "Beto", anonymitySet: 1),
+		};
+		var semiPrivateAndPrivateCoins = smartCoins.Where(x => x.HdPubKey.AnonymitySet >= 2).Select(x => x.Coin);
+
+		var selector = new SmartCoinSelector(smartCoins, privateThreshold, recipient: "");
+		var coinsToSpend = selector.Select(Enumerable.Empty<Coin>(), amountToSend).Cast<Coin>().ToList();
+
+		Assert.True(coinsToSpend.All(coin => semiPrivateAndPrivateCoins.Contains(coin)));
+	}
+
+	[Theory]
+	[InlineData("Lucas")]
+	[InlineData("David")]
+	[InlineData("Lucas, David")]
+	public void PreferCoinsThatKnownByTheRecipient(SmartLabel recipient)
+	{
+		var privateThreshold = 100;
+		var amountToSend = Money.Coins(0.5m);
+
+		var smartCoins = new List<SmartCoin>()
+		{
+			LabelTestExtensions.CreateCoin(1m, "Juan"),
+			LabelTestExtensions.CreateCoin(1m, "Beto"),
+			LabelTestExtensions.CreateCoin(1m, "Lucas"),
+			LabelTestExtensions.CreateCoin(1m, "David"),
+			LabelTestExtensions.CreateCoin(1m, "Lucas, David"),
+		};
+		var knownByRecipientCoins = smartCoins.Where(x => x.HdPubKey.Label == recipient).Select(x => x.Coin);
+
+		var selector = new SmartCoinSelector(smartCoins, privateThreshold, recipient);
+		var coinsToSpend = selector.Select(Enumerable.Empty<Coin>(), amountToSend).Cast<Coin>().ToList();
+
+		Assert.True(coinsToSpend.All(coin => knownByRecipientCoins.Contains(coin)));
 	}
 
 	private IEnumerable<SmartCoin> GenerateSmartCoins(IEnumerable<(string Cluster, decimal amount)> coins)
