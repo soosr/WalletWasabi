5806e1e610628f087d09e7ce7cb1b3fdd30b74ec
Roland Soós <soos.roland93@gmail.com>
2024-01-02 14:32:59 +0100

    better approach

---
+++ WalletWasabi.Fluent/Converters/RangeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Linq;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace WalletWasabi.Fluent.Converters;
+
+public class RangeConverter : IValueConverter
+{
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		if (value is IEnumerable list && parameter is string range)
+		{
+			var parts = range.Split(new[] { ".." }, StringSplitOptions.None);
+			if (parts.Length == 2 && int.TryParse(parts[0], out int start) && int.TryParse(parts[1], out int end))
+			{
+				var count = end - start + 1;
+				return list.Cast<object>().Skip(start).Take(count).ToList();
+			}
+		}
+
+		return AvaloniaProperty.UnsetValue;
+	}
+
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException(); // Not needed for one-way binding
+	}
+}
